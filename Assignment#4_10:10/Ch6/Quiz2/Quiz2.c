155760ccbfcab8b888e6b7d995cbe9064d68f621
Sangwoo Jung <s9779j@gmail.com>
2021-10-10 13:43:47 +0900

    Title Ch6_Quiz2 Description 입력받은 달의 일수를 계산 if else 사용

--- Assignment#4_10:10/Ch6/Quiz2/Quiz2.c
+++ Assignment#4_10:10/Ch6/Quiz2/Quiz2.c
@@ -1,5 +1,5 @@
 // 입력받은 달의 일수를 계산
-// switch 사용
+// if else 사용
 
 #include <stdio.h>
 
@@ -10,22 +10,14 @@ int main(void)
     printf("달을 입력하시오: ");
     scanf("%d", &month);
 
-    switch (month)
-    {
-        case 2:
-            printf("%d월의 일수는 %d입니다.\n", month, 28);
-            break;
-        
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            printf("%d월의 일수는 %d입니다\n", month, 30);
-            break;
+    if (month==2)
+        printf("%d월의 일수는 %d입니다.\n", month, 28);
+    
+    else if (month==4 || month==6 || month==9 || month==11)
+        printf("%d월의 일수는 %d입니다.\n", month, 30);
 
-        default:
-            printf("%d월의 일수는 %d입니다.\n", month, 31);
-    }
+    else
+        printf("%d월의 일수는 %d입니다.\n", month, 31);
 
     return 0;
 }
