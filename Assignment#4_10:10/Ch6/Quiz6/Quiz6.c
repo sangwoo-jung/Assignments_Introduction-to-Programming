f1213a5b0b2fbf8fc7133226fe496dde5e6afe25
Sangwoo Jung <s9779j@gmail.com>
2021-10-10 15:26:54 +0900

    Title Ch6_Quiz6 Description 어떤 점의 x좌표 와 y좌표를 각각 받아 그 점이 몇 사분면에 속하는지 판단 conditional operator 사용

--- Assignment#4_10:10/Ch6/Quiz6/Quiz6.c
+++ Assignment#4_10:10/Ch6/Quiz6/Quiz6.c
@@ -1,5 +1,5 @@
 // 어떤 점의 x좌표 와 y좌표를 각각 받아 그 점이 몇 사분면에 속하는지 판단
-// if else 사용
+// conditional operator 사용
 
 #include <stdio.h>
 
@@ -12,14 +12,15 @@ int main(void)
     printf("y좌표를 입력하시오: ");
     scanf("%d", &y);
 
-    if ( x>0 && y>0 )
-        printf("제1 사분면\n");
-    else if ( x<0 && y>0 )
-        printf("제2 사분면\n");
-    else if ( x<0 && y<0 )
-        printf("제3 사분면\n");
-    else if ( x>0 && y<0 )
-        printf("제4 사분면\n");
+    ( x>0 && y>0 )?
+    printf("제1 사분면\n"):
+    ( x<0 && y>0 )?
+    printf("제2 사분면\n"):
+    ( x<0 && y<0 )?
+    printf("제3 사분면\n"):
+    ( x>0 && y<0 )?
+    printf("제4 사분면\n"):
+    printf("None");
 
     return 0;
 }
