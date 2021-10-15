2bae9207de24d648c5b19ec72c29a38da3f134fb
Sangwoo Jung <92249339+sangwoo-jung@users.noreply.github.com>
2021-10-16 00:04:12 +0900

    Ttile 음이 아닌 정수 n을 입력받아 n이 0이면 종료하고 n이 0이 아니면 1~n까지 홀수들과 짝수들의 역원들을 모두 더하라 Description for, break 사용, sum printf 하나의 block으로 묶음

--- Assignment#5_10-17/Ch7/Quiz3/Quiz3.c
+++ Assignment#5_10-17/Ch7/Quiz3/Quiz3.c
@@ -1,5 +1,5 @@
 // 음이 아닌 정수 n을 입력받아 n이 0이면 종료하고 n이 0이 아니면 1~n까지 홀수들과 짝수들의 역원들을 모두 더하라
-// while, break 사용, sum printf 하나의 block으로 묶음
+// for, break 사용, sum printf 하나의 block으로 묶음
 
 #include <stdio.h>
 
@@ -7,40 +7,37 @@ int main(void)
 {
     int n=0;
 
-    while (1)
+    for (int i=1; 1; i++)
     {
         printf("n을 입력하시오: ");
         scanf("%d", &n);
 
         if ( n==0 )
             break;
-        
+
         else 
         {
             int sum=0;
-            int num=1;
-            while (num<=n)
+
+            for (int num=1; num<=n; num++)
             {
                 if (num%2!=0)
-                {   
+                {    
                     sum=sum+num;
 
                     if (num==1)
                         printf("%d", 1);
-                    else 
+                    else
                         printf("+%d", num);
                 }
-
                 else
                 {    
                     sum=sum+(-num);
 
                     printf("-%d", num);
                 }    
-
-                num++;
             }
-            
+
             printf("=%d\n\n", sum);
         }
     }
