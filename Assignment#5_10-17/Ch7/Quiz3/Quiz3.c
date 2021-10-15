1e41483465a3ba4176143620420203a85a5762b6
Sangwoo Jung <92249339+sangwoo-jung@users.noreply.github.com>
2021-10-15 20:49:16 +0900

    Title 음이 아닌 정수 n을 입력받아 n이 0이면 종료하고 n이 0이 아니면 1~n까지 홀수들과 짝수들의 역원들을 모두 더하라 Description while, break 사용

--- Assignment#5_10-17/Ch7/Quiz3/Quiz3.c
+++ Assignment#5_10-17/Ch7/Quiz3/Quiz3.c
@@ -1,5 +1,5 @@
 // 음이 아닌 정수 n을 입력받아 n이 0이면 종료하고 n이 0이 아니면 1~n까지 홀수들과 짝수들의 역원들을 모두 더하라
-// for, break 사용
+// while, break 사용
 
 #include <stdio.h>
 
@@ -7,7 +7,7 @@ int main(void)
 {
     int n=0;
 
-    for (int i=1; 1; i++)
+    while (1)
     {
         printf("n을 입력하시오: ");
         scanf("%d", &n);
@@ -18,23 +18,30 @@ int main(void)
         else 
         {
             int sum=0;
+            int num=1;
 
-            for (int num=1; num<=n; num++)
+            while (num<=n)
             {
                 if (num%2!=0)
                     sum=sum+num;
                 else
                     sum=sum+(-num);
+
+                num++;
             }
 
             printf("%d", 1);
-            for (int flag=2; flag<=n; flag++)
+            int flag=2;
+            while (flag<=n)
             {
                 if (flag%2!=0)
                     printf("+%d", flag);
                 else 
                     printf("-%d", flag);
+
+                flag++;
             }
+            
             printf("=%d", sum);
 
             printf("\n\n");
